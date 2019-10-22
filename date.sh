6da0c6fc5622b827396398af604ef7a84b0fb92c
Qoli Wong <llqoli@gmail.com>
2019-10-22 15:15:19 +0800

    1.0.4

--- date.sh
+++
@@ -1,2 +0,0 @@
- #!/bin/bash
- date "+%a, %d %b %Y %H:%M:%S %z"
--- sign.sh
+++ sign.sh
@@ -1,2 +1,4 @@
  #!/bin/bash
+ date "+%a, %d %b %Y %H:%M:%S %z"
+ echo ""
  /Users/qoli/Applications/Sparkle/bin/sign_update ./Release/aDictMac.zip 
