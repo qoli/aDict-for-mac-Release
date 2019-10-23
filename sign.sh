74264bc0dac8467618479c65dafec0c457f0eae4
Qoli Wong <llqoli@gmail.com>
2019-10-23 10:26:48 +0800

    1.0.4

--- sign.sh
+++ sign.sh
@@ -1,4 +1,6 @@
  #!/bin/bash
+ ditto -c -k --sequesterRsrc --keepParent "./app/aDict for mac.app" "./Release/aDictMac.zip"
  date "+%a, %d %b %Y %H:%M:%S %z"
  echo ""
  /Users/qoli/Applications/Sparkle/bin/sign_update ./Release/aDictMac.zip 
+ cot ./appcast.xml
