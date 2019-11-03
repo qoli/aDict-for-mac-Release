f74e688c69cae602de458cf6a8a4a8b4f6d6e2c0
Qoli Wong <llqoli@gmail.com>
2019-11-03 22:21:19 +0800

    1.1
    合併源碼和修復潛在 Bug

--- sign.sh
+++ sign.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
- ditto -c -k --sequesterRsrc --keepParent "./app/aDict for mac.app" "./Release/aDictMac.zip"
+ ditto -c -k --sequesterRsrc --keepParent "./app/aDict for macOS.app" "./Release/aDictMac.zip"
  date "+%a, %d %b %Y %H:%M:%S %z"
  echo ""
  /Users/qoli/Applications/Sparkle/bin/sign_update ./Release/aDictMac.zip 
